431e2bced108e033cba2881c1b1483687fa90d85
Warintorn <5810110302@psu.ac.th>
2018-09-19 14:14:23 +0700

    Add Install Commands

--- install_tools.sh
+++ install_tools.sh
@@ -2,4 +2,11 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt -u dist-upgrade
 
+sudo apt install build-essential
+sudo apt -y install nodejs
+npm install -g yarn
+npm install -g pm2
+
+sudo apt -y install mariadb-server
+
 sudo apt -y autoremove
